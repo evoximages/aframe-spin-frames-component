ed23e906b8031d8eb934da52d786b1cc40874e62
Ben Li <benbli@gmail.com>
2019-03-19 10:13:47 -0700

    texture loader

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ AFRAME.registerComponent('spin-frames', {
   multiple: true,
   schema: {
     // configurable options
+    vifnum: { type: 'string' },
     folder: { type: 'string' },
     sensitivity: { default: 3.2 },
     frameIndex: { type: 'number', default: 24 },
@@ -11,234 +12,249 @@ AFRAME.registerComponent('spin-frames', {
     // default flags
     loading: { default: true },
     enabled: { default: true },
-    initTick: { type: 'boolean', default: false },
+    initTick: { type: 'boolean', default: false }
   },
   init: function() {
-    this.textures = []
-    this.IMAGECOUNT = 36
-    this.FRAMES = 88
-    this.COUNTER = 2112 // starting image * FRAMES
+    this.textures = [];
+    this.IMAGECOUNT = 36;
+    this.FRAMES = 88;
+    this.COUNTER = 2112; // starting image * FRAMES
 
-    this.startX = 0
-    this.lookVector = new THREE.Vector2()
-    this.mouseDown = false
-    this.touchDown = false
-    this.bindMethods()
+    this.startX = 0;
+    this.lookVector = new THREE.Vector2();
+    this.mouseDown = false;
+    this.touchDown = false;
+    this.bindMethods();
   },
 
   update: function() {
-    this.loadImages()
-    this.updateMeshTexture(this.data.frameIndex)
-    this.setStereoLayer()
+    this.loadImages();
+    this.updateMeshTexture(this.data.frameIndex);
+    this.setStereoLayer();
   },
 
   play: function() {
-    this.addEventListeners()
+    this.addEventListeners();
   },
 
   pause: function() {
-    this.removeEventListeners()
-    this.lookVector.set(0, 0)
+    this.removeEventListeners();
+    this.lookVector.set(0, 0);
   },
 
   remove: function() {
-    this.pause()
+    this.pause();
   },
 
   bindMethods: function() {
-    this.onMouseDown = this.onMouseDown.bind(this)
-    this.onMouseMove = this.onMouseMove.bind(this)
-    this.onMouseUp = this.onMouseUp.bind(this)
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
 
-    this.onTouchStart = this.onTouchStart.bind(this)
-    this.onTouchMove = this.onTouchMove.bind(this)
-    this.onTouchEnd = this.onTouchEnd.bind(this)
+    this.onTouchStart = this.onTouchStart.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.onTouchEnd = this.onTouchEnd.bind(this);
 
-    this.setStereoLayer = this.setStereoLayer.bind(this)
-    this.onExitVr = this.onExitVr.bind(this)
-    this.onEnterVr = this.onEnterVr.bind(this)
+    this.setStereoLayer = this.setStereoLayer.bind(this);
+    this.onExitVr = this.onExitVr.bind(this);
+    this.onEnterVr = this.onEnterVr.bind(this);
   },
 
   addEventListeners: function() {
-    const canvasEl = this.el.sceneEl.canvas
-    const aScene = document.querySelector('a-scene')
+    const canvasEl = this.el.sceneEl.canvas;
+    const aScene = document.querySelector('a-scene');
     // Mouse events
-    canvasEl.addEventListener('mousedown', this.onMouseDown, false)
-    canvasEl.addEventListener('mousemove', this.onMouseMove, false)
-    canvasEl.addEventListener('mouseup', this.onMouseUp, false)
+    canvasEl.addEventListener('mousedown', this.onMouseDown, false);
+    canvasEl.addEventListener('mousemove', this.onMouseMove, false);
+    canvasEl.addEventListener('mouseup', this.onMouseUp, false);
 
     // Touch events
-    canvasEl.addEventListener('touchstart', this.onTouchStart, false)
-    canvasEl.addEventListener('touchmove', this.onTouchMove, false)
-    canvasEl.addEventListener('touchend', this.onTouchEnd, false)
+    canvasEl.addEventListener('touchstart', this.onTouchStart, false);
+    canvasEl.addEventListener('touchmove', this.onTouchMove, false);
+    canvasEl.addEventListener('touchend', this.onTouchEnd, false);
 
-    aScene.addEventListener('enter-vr', this.onEnterVr, false)
-    aScene.addEventListener('exit-vr', this.onExitVr, false)
+    aScene.addEventListener('enter-vr', this.onEnterVr, false);
+    aScene.addEventListener('exit-vr', this.onExitVr, false);
   },
 
   removeEventListeners: function() {
-    const canvasEl = this.el.sceneEl && this.el.sceneEl.canvas
+    const canvasEl = this.el.sceneEl && this.el.sceneEl.canvas;
     if (canvasEl) {
-      canvasEl.removeEventListener('mousedown', this.onMouseDown)
-      canvasEl.removeEventListener('mousemove', this.onMouseMove)
-      canvasEl.removeEventListener('mouseup', this.onMouseUp)
+      canvasEl.removeEventListener('mousedown', this.onMouseDown);
+      canvasEl.removeEventListener('mousemove', this.onMouseMove);
+      canvasEl.removeEventListener('mouseup', this.onMouseUp);
 
-      canvasEl.removeEventListener('touchstart', this.onTouchStart)
-      canvasEl.removeEventListener('touchmove', this.onTouchMove)
-      canvasEl.removeEventListener('touchend', this.onTouchEnd)
+      canvasEl.removeEventListener('touchstart', this.onTouchStart);
+      canvasEl.removeEventListener('touchmove', this.onTouchMove);
+      canvasEl.removeEventListener('touchend', this.onTouchEnd);
     }
   },
 
   tick: function(time, delta) {
     if (this.data.initTick) {
-      this.updateImageByFrame(time, delta)
+      this.updateImageByFrame(time, delta);
     }
   },
 
+  // loadImages: function() {
+  //   this.textures = []
+  //   const loader = new THREE.TextureLoader()
+  //   for (let i = 0; i < this.IMAGECOUNT; i++) {
+  //     this.textures.push(loader.load(`${this.data.folder + i}.png`))
+  //     //   this.textures.push(loader.load(this.data.folder[i]))
+  //   }
+  // },
+
   loadImages: function() {
-    this.textures = []
-    const loader = new THREE.TextureLoader()
-    for (let i = 0; i < this.IMAGECOUNT; i++) {
-      this.textures.push(loader.load(`${this.data.folder + i}.png`))
-      //   this.textures.push(loader.load(this.data.folder[i]))
+    const loader = new THREE.TextureLoader();
+    this.textures = [];
+    for (let i = 10; i <= 360; i += 10) {
+      let num = i.toString();
+      let zeroString = '000';
+      let padded = zeroString.substring(num.length, 4) + num;
+      this.textures.push(
+        loader.load(
+          `${this.data.folder}AIL${this.data.vifnum}_left_${padded}.png`
+        )
+      );
     }
   },
 
   updateMeshTexture: function(index) {
-    const mesh = this.el.getObject3D('mesh')
-    if (!mesh || !mesh.material) return
-    mesh.material.map = this.textures[index]
+    const mesh = this.el.getObject3D('mesh');
+    if (!mesh || !mesh.material) return;
+    mesh.material.map = this.textures[index];
   },
 
   updateImageByFrame: function(time, delta) {
     if (!this.data.clickToSpin) {
       // Calculate rotation if dragging
-      this.COUNTER += Math.round(time)
+      this.COUNTER += Math.round(time);
       // Avoid negative modulus
       this.data.frameIndex =
         ((Math.round(this.COUNTER * (1 / this.FRAMES)) % this.IMAGECOUNT) +
           this.IMAGECOUNT) %
-        this.IMAGECOUNT
+        this.IMAGECOUNT;
     } else {
       // Calculate rotation for auto spin
-      this.COUNTER += Math.round(delta)
+      this.COUNTER += Math.round(delta);
       this.data.frameIndex =
-        Math.round(this.COUNTER * (1 / this.FRAMES)) % this.IMAGECOUNT
+        Math.round(this.COUNTER * (1 / this.FRAMES)) % this.IMAGECOUNT;
     }
-    this.updateMeshTexture(this.data.frameIndex)
+    this.updateMeshTexture(this.data.frameIndex);
   },
 
   isRotationActive: function() {
-    return this.data.enabled && (this.mouseDown || this.touchDown)
+    return this.data.enabled && (this.mouseDown || this.touchDown);
   },
 
   rotateObject: function(clientX) {
-    if (clientX === this.startX) return
+    if (clientX === this.startX) return;
 
-    const currentX = clientX
-    let direction = 1
+    const currentX = clientX;
+    let direction = 1;
 
     if (currentX > this.startX) {
-      direction = -1
+      direction = -1;
     }
 
     const amountMoved =
-      Math.abs(currentX - this.startX) * direction * this.data.sensitivity
-    this.updateImageByFrame(amountMoved)
-    this.startX = currentX
+      Math.abs(currentX - this.startX) * direction * this.data.sensitivity;
+    this.updateImageByFrame(amountMoved);
+    this.startX = currentX;
   },
 
   onMouseMove: function(event) {
-    if (!this.data.enabled || !this.mouseDown || this.data.clickToSpin) return
+    if (!this.data.enabled || !this.mouseDown || this.data.clickToSpin) return;
 
-    const previousMouseEvent = this.previousMouseEvent
+    const previousMouseEvent = this.previousMouseEvent;
 
-    let movementX
-    movementX = event.movementX || event.mozMovementX || 0
+    let movementX;
+    movementX = event.movementX || event.mozMovementX || 0;
 
     if (movementX === undefined) {
-      movementX = event.screenX - previousMouseEvent.screenX
+      movementX = event.screenX - previousMouseEvent.screenX;
     }
-    this.previousMouseEvent = event
+    this.previousMouseEvent = event;
 
     if (this.isRotationActive()) {
-      this.lookVector.x += movementX
-      this.rotateObject(this.lookVector.x)
+      this.lookVector.x += movementX;
+      this.rotateObject(this.lookVector.x);
     }
   },
 
   onMouseDown: function(event) {
-    this.mouseDown = true
-    this.previousMouseEvent = event
+    this.mouseDown = true;
+    this.previousMouseEvent = event;
   },
 
   onMouseUp: function() {
-    this.mouseDown = false
-    if (!this.data.clickToSpin) return
+    this.mouseDown = false;
+    if (!this.data.clickToSpin) return;
     this.data.initTick
       ? (this.data.initTick = false)
-      : (this.data.initTick = true)
+      : (this.data.initTick = true);
   },
 
   // TOUCH CONTROLS
   onTouchMove: function(event) {
-    if (!this.data.enabled || !this.touchDown) return
+    if (!this.data.enabled || !this.touchDown) return;
 
-    const previousTouchEvent = this.previousTouchEvent
-    const touch = event.touches[0]
-    const movementX = touch.screenX - previousTouchEvent.touches[0].screenX
+    const previousTouchEvent = this.previousTouchEvent;
+    const touch = event.touches[0];
+    const movementX = touch.screenX - previousTouchEvent.touches[0].screenX;
 
-    this.previousTouchEvent = event
+    this.previousTouchEvent = event;
 
     if (this.isRotationActive()) {
-      this.lookVector.x += movementX
-      this.rotateObject(this.lookVector.x)
+      this.lookVector.x += movementX;
+      this.rotateObject(this.lookVector.x);
     }
   },
 
   onTouchStart: function(event) {
-    this.touchDown = true
-    this.previousTouchEvent = event
+    this.touchDown = true;
+    this.previousTouchEvent = event;
   },
 
   onTouchEnd: function() {
-    this.touchDown = false
+    this.touchDown = false;
   },
 
   onEnterVr: function() {
-    this.setStereoLayer('enter-vr')
+    this.setStereoLayer('enter-vr');
   },
 
   onExitVr: function() {
-    window.location.reload(true)
+    window.location.reload(true);
   },
 
   setStereoLayer: function(event) {
-    const obj3d = this.el.object3D.children[0]
+    const obj3d = this.el.object3D.children[0];
 
     if (this.data.stereo === 'left') {
-      obj3d.layers.set(0)
+      obj3d.layers.set(0);
     }
 
     if (this.data.stereo === 'right') {
-      obj3d.layers.set(2)
-      obj3d.visible = false
+      obj3d.layers.set(2);
+      obj3d.visible = false;
     }
 
     if (event === 'enter-vr') {
       if (this.data.stereo === 'left') {
-        obj3d.layers.set(1)
+        obj3d.layers.set(1);
       }
       if (this.data.stereo === 'right') {
-        obj3d.visible = true
-        obj3d.layers.set(2)
+        obj3d.visible = true;
+        obj3d.layers.set(2);
       }
     }
 
     if (event === 'exit-vr') {
       if (this.data.stereo === 'right') {
-        obj3d.visible = false
+        obj3d.visible = false;
       }
     }
-  },
-})
+  }
+});
