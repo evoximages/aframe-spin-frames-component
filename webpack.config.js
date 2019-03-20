141650d65d3d3ab4fd14db7f946273906f137cc5
Ben Li <benbli@gmail.com>
2019-03-20 13:39:21 -0700

    adds webpack config for transpiler

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpack_rules = [];
+const webpackOption = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'aframe-spin-frames-component.js'
+  },
+  module: {
+    rules: webpack_rules
+  }
+};
+let babelLoader = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+    }
+  }
+};
+webpack_rules.push(babelLoader);
+module.exports = webpackOption;
